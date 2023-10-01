b04ec82d2da8f53d6a5775a2c3ae746f9730aa91
ijestanoff <jestanoff@icloud.com>
2023-10-01 21:10:08 +0300

    remove 06.EqualSum

--- Java_Script_Fundamentals/ArraysExercise/06EqualSums.js
+++ Java_Script_Fundamentals/ArraysExercise/06EqualSums.js
@@ -21,4 +21,4 @@ equalSums ([1, 2, 3, 3]);
 equalSums ([1, 2]);
 equalSums ([1]);
 equalSums ([1, 2, 3]);
-//equalSums ([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4]);
+equalSums ([10, 5, 5, 99, 3, 4, 2, 5, 1, 1, 4]);
