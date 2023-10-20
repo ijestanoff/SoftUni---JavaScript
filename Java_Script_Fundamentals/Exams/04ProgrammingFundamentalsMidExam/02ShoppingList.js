eef3a4af6cbe0248ed4ac416d244cd98d60ea744
ijestanoff <jestanoff@icloud.com>
2023-10-20 19:51:46 +0300

    MidExam 4.2 update

--- Java_Script_Fundamentals/Exams/04ProgrammingFundamentalsMidExam/02ShoppingList.js
+++ Java_Script_Fundamentals/Exams/04ProgrammingFundamentalsMidExam/02ShoppingList.js
@@ -1,32 +1,14 @@
 function shoppingList(input) {
     let shopList = input.shift().split('!');
     for (let part of input) {
-        let commands = part.split(' ');
-        if (commands[1] == 'Shopping!') break;
-        let command = commands[0];
-        let item = commands[1];
-        if (command == 'Urgent') {
-            if (!shopList.includes(item)) {
-                shopList.unshift(item);
-            }
-        } else if (command == 'Unnecessary') {
-            if (shopList.includes(item)) {
-                let i = shopList.indexOf(item);
-                shopList.splice(i, 1);
-            }
-        } else if (command == 'Correct') {
-            let newItem = commands[2];
-            if (shopList.includes(item)) {
-                let i = shopList.indexOf(item);
-                shopList[i] = newItem;
-            }
-        } else if (command == 'Rearrange') {
-            if (shopList.includes(item)) {
-                let i = shopList.indexOf(item);
-                let temp = shopList[i];
-                shopList.splice(i, 1);
-                shopList.push(temp);
-            }
+        let [command, item, correct] = part.split(' ');
+        if (command == 'Shopping!') break;
+        if (command == 'Urgent' && !shopList.includes(item)) shopList.unshift(item);
+        else if (shopList.includes(item)) {
+            let i = shopList.indexOf(item);
+            if (command == 'Unnecessary') shopList.splice(i, 1);
+            if (command == 'Correct') shopList[i] = correct;
+            if (command == 'Rearrange') shopList.push(shopList.splice(i, 1));
         }
     }
     console.log(shopList.join(', '));
