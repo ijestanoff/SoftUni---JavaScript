0d738923b5c70ed6128c6f8e954d9f565951bd6c
ijestanoff <jestanoff@icloud.com>
2023-10-24 22:36:27 +0300

    ProgrammingFundamentalsFinalExamRetake 2(30%)

---
+++ Java_Script_Fundamentals/Exams/01ProgrammingFundamentalsFinalExamRetake/02AdAstra.js
@@ -0,0 +1,9 @@
+function adAstra () {
+    let totalCalories = 0;
+    let regex = /([#|])[A-Za-z\s]+\1\d{2}\/\d{2}\/\d{2}\1(?<amount>\d+)\1/g;
+    
+}
+
+adAstra (['#Bread#19/03/21#4000#|Invalid|03/03.20||Apples|08/10/20|200||Carrots|06/08/20|500||Not right|6.8.20|5|']);
+adAstra (['$$#@@%^&#Fish#24/12/20#8500#|#Incorrect#19.03.20#450|$5*(@!#Ice Cream#03/10/21#9000#^#@aswe|Milk|05/09/20|2000|']);
+adAstra (['Hello|#Invalid food#19/03/20#450|$5*(@' ]);
