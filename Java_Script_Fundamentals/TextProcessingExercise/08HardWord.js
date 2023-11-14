94f7b35d17412d6a582dc0943500380b1f766e46
ijestanoff <jestanoff@icloud.com>
2023-11-14 22:41:12 +0200

    TextProcessingExercise 8 update

--- Java_Script_Fundamentals/TextProcessingExercise/08HardWord.js
+++ Java_Script_Fundamentals/TextProcessingExercise/08HardWord.js
@@ -1,9 +1,9 @@
-function hardWord (array) {
+function hardWord(array) {
     let letter = array[0].split(' ');
     let words = array[1];
     for (let word of words) {
         let repeated = '_'.repeat(word.length);
-        for (let index=0; index<letter.length - 1; index++) {
+        for (let index = 0; index < letter.length - 1; index++) {
             let long = repeated.length;
             if (letter[index].startsWith(repeated) && letter[index][long] != '_') {
                 let symbol = '';
@@ -17,4 +17,4 @@ function hardWord (array) {
     console.log(letter.join(' '));
 }
 
-hardWord (['Hi, grandma! I\'m so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother\'s ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.', ['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']]);
+hardWord(['Hi, grandma! I\'m so ____ to write to you. ______ the winter vacation, so _______ things happened. My dad bought me a sled. Mom started a new job as a __________. My brother\'s ankle is ________, and now it bothers me even more. Every night Mom cooks ___ on your recipe because it is the most delicious. I hope this year Santa will _____ me a robot.', ['pie', 'bring', 'glad', 'During', 'amazing', 'pharmacist', 'sprained']]);
