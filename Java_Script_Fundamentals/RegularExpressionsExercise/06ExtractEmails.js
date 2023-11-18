64f83e7e007006166d412eec47fa56e11877884a
ijestanoff <jestanoff@icloud.com>
2023-11-18 14:30:28 +0200

    RegularExpressionsExercise 6 update

--- Java_Script_Fundamentals/RegularExpressionsExercise/06ExtractEmails.js
+++ Java_Script_Fundamentals/RegularExpressionsExercise/06ExtractEmails.js
@@ -1,9 +1,6 @@
 function extractEmails(input) {
-    let regex = /(^|(?<=\s))([a-z0-9]+[\._-]?\w+@([a-z]+(\-?[a-z]+(\.[a-z]+)+)+))(\b|(?=\s))/gi;
-    let match = input.match(regex);
-    if (match) {
-        Object.values(match).forEach(email => console.log(email));
-    }
+    let match = input.match(/(^|(?<=\s))([a-z0-9]+[\._-]?\w+@([a-z]+(\-?[a-z]+(\.[a-z]+)+)+))(\b|(?=\s))/gi);
+    if (match) Object.values(match).forEach(email => console.log(email));
 }
 
 extractEmails('Please contact us at: support@github.com.');
