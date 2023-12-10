34317647e67aa2019de393ef69caf57ad20f918e
ijestanoff <jestanoff@icloud.com>
2023-12-10 11:56:26 +0200

    task35 codewar 30%

--- Java_Script_Fundamentals/CodeWars/task35.js
+++ Java_Script_Fundamentals/CodeWars/task35.js
@@ -1,6 +1,16 @@
 function findPosition(num) {
-
-
+    let idx = '12345678910111213141516'.indexOf(num);
+    if (idx != -1) return idx;
+    for (let step = 2; step < num.length; step++) {
+        for (let offset = 0; offset < step; offset++) {
+            let n = [];
+            let init = num.slice(0, offset);
+            if (init.length) n.push(init);
+            for (let idx = offset; idx < num.length; idx += step) {
+                n.push(num.slice(idx, idx + step));
+            }
+        }
+    }
 }
 
 const position = (number) => {
@@ -10,9 +20,10 @@ const position = (number) => {
         9: 788888888, 10: 8888888888, 11: 98888888888, 12: 1088888888888,
         13: 11888888888888, 14: 128888888888888, 15: 1388888888888888
     };
-    let out = offset[len];
-    let mid = (number - Number('9'.repeat(len - 1))) * len
-    return out + mid;
+    return offset[len] + (number - 1 - Number('9'.repeat(len - 1))) * len + 1;
 }
 
-console.log(position(99));
+console.log(findPosition('12345670899'));
+console.log(position(119855589995974));
+
+//"555899959741198" , 1686722738828503
