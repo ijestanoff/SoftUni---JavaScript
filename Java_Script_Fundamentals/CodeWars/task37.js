f4f19428f29ce29fbc508cc19b8715a776a81cfd
ijestanoff <jestanoff@icloud.com>
2023-12-16 18:53:43 +0200

    task38 codewar 10%

--- Java_Script_Fundamentals/CodeWars/task37.js
+++ Java_Script_Fundamentals/CodeWars/task37.js
@@ -1,15 +1,17 @@
-function searchSubstr( fullText, searchText, allowOverlap = true ){
-    if (searchText=='') return 0;
-    if (allowOverlap) {
-        return Array.from(fullText.matchAll(new RegExp(`(?=(${searchText}))`, "g")), x => x[1]).length;
-    } else {
-        return fullText.match(new RegExp(searchText, "g")).length;
-    }
+// function searchSubstr(fullText, searchText, allowOverlap = true) {
+//     if (searchText == '') return 0;
+//     if (allowOverlap) {
+//         return Array.from(fullText.matchAll(new RegExp(`(?=(${searchText}))`, "g")), x => x[1]).length;
+//     } else {
+//         return fullText.match(new RegExp(searchText, "g")).length;
+//     }
+// }
+
+function searchSubstr(fullText, searchText, allowOverlap) {
+    return fullText && searchText ? fullText.match(new RegExp(allowOverlap ? `(?=${searchText})` : searchText, `g`) || []).length : 0;
 }
-
-
 console.log(searchSubstr('aa_bb_cc_dd_bb_e', 'bb'));//, 2
 console.log(searchSubstr('aaabbbcccc', 'bbb'));//, 1
-console.log(searchSubstr( 'aaa', 'aa' ));//, 2
-console.log(searchSubstr( 'aaa', '' ));//, 0
-console.log(searchSubstr( 'aaa', 'aa', false ));//, 1
+console.log(searchSubstr('aaa', 'aa'));//, 2
+console.log(searchSubstr('aaa', ''));//, 0
+console.log(searchSubstr('aaa', 'aa', false));//, 1
---
+++ Java_Script_Fundamentals/CodeWars/task38.js
@@ -0,0 +1,7 @@
+function sumIntervals(intervals) {
+    let int = [intervals.shift()];
+    console.log(int);
+}
+
+console.log(sumIntervals([[1, 5], [1, 5]]));// 4
+console.log(sumIntervals([[1, 4], [7, 10], [3, 5]]));// 7
